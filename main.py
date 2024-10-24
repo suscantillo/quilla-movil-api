e68fd946363743e7a787f4a296f2de328d899528
suscantillo <jesusguerrero@uninorte.edu.co>
2024-10-24 17:09:26 -0500

    pepe

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from .database import Base, engine, get_db
-from .models import Location
+from database import Base, engine, get_db
+from models import Location
 
 Base.metadata.create_all(bind=engine)
 
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 
 from sqlalchemy import Column, Integer, Float, String
-from .database import Base
+from database import Base
 
 class Location(Base):
     __tablename__ = "locations"
